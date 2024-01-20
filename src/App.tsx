45b836a82e0a3597216f7e8021ae872b151372b1
Mayank Rastogi <mayankrustey2002@gmail.com>
2024-01-20 14:09:21 -0800

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import '@mantine/core/styles.css';
 import ColorSchemeContext from './ColorSchemeContext';
 import { MantineProvider } from '@mantine/core';
-import { Textarea } from '@mantine/core';
 import LightDarkButton from './components/LightDarkButton';
 import { useWindowEvent, useLocalStorage } from '@mantine/hooks';
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ErrorPage from "./components/ErrorPage.tsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/error",
+    element: <ErrorPage />,
+  },
+  {
+    path: "/test",
+    element: <div>Test</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
