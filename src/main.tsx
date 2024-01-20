4504d4b31f676230b635e19cd3a78f760dc08368
Mayank Rastogi <mayankrustey2002@gmail.com>
2024-01-20 14:22:55 -0800

    Configured routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,9 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Login from './pages/Login.tsx';
+import Profile from './pages/Profile.tsx';
+import Chat from './pages/Chat.tsx';
 
 const router = createBrowserRouter([
   {
@@ -17,6 +20,18 @@ const router = createBrowserRouter([
   {
     path: "/error",
     element: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/chat",
+    element: <Chat />,
   },
   {
     path: "/test",
