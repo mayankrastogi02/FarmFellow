1e54e3613a139adb53a698a8a0f02fc84c5975f1
Mayank Rastogi <mayankrustey2002@gmail.com>
2024-01-21 01:29:23 -0800

    Created shared components and connected navigation

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,12 +1,24 @@
-const ProfileCard = (userData) => {
-    // const { age, email, first_name, last_name, gender, height, phone, profile_picture, weight } = userData
-
-    console.log(userData)
+import React from 'react'
+import cardImg from '../assets/cardImg.jpg'
+import { useNavigate } from 'react-router-dom'
+function ProfileCard() {
+    const navigate = useNavigate();
     return (
-        <div className="bg-darkBackground bg-opacity-5">
-            <div>
-                {/* <p>{first_name} {last_name}</p> */}
-
+        <div className="card w-96 shadow-xl p-0 hover:shadow-2xl" onClick={() => { navigate('/profile') }} role='button' >
+            <figure><img src={cardImg} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    Steveston Farm
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p className='text-left'>If a dog chews shoes whose shoes does he choose?</p>
+                <p className='text-left'>Steveston, BC</p>
+                <div className="card-actions justify-start">
+                    <div className="badge badge-outline">Kale</div>
+                    <div className="badge badge-outline">Carrots</div>
+                    <div className="badge badge-outline">Cucumbers</div>
+                    <div className="badge badge-outline">Lettuce</div>
+                </div>
             </div>
         </div>
     )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import cardImg from '../assets/cardImg.jpg'
+import ProfileCard from '../components/ProfileCard';
 function HomePage() {
     const arr = [1, 2, 3, 4, 5];
     return (
@@ -11,23 +11,7 @@ function HomePage() {
                 {
                     arr.map((item) => {
                         return (
-                            <div className="card w-96 shadow-xl p-0" >
-                                <figure><img src={cardImg} alt="Shoes" /></figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">
-                                        Steveston Farm
-                                        <div className="badge badge-secondary">NEW</div>
-                                    </h2>
-                                    <p className='text-left'>If a dog chews shoes whose shoes does he choose?</p>
-                                    <p className='text-left'>Steveston, BC</p>
-                                    <div className="card-actions justify-start">
-                                        <div className="badge badge-outline">Kale</div>
-                                        <div className="badge badge-outline">Carrots</div>
-                                        <div className="badge badge-outline">Cucumbers</div>
-                                        <div className="badge badge-outline">Lettuce</div>
-                                    </div>
-                                </div>
-                            </div>
+                            <ProfileCard />
                         )
                     })
                 }
