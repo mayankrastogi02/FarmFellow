a11b2ed5efd8b1bb128a0d671f7c1e9113fc4651
Mayank Rastogi <mayankrustey2002@gmail.com>
2024-01-21 11:56:44 -0800

    Landing page changed

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Care from '../assets/care.svg'
+import Landing2 from '../assets/landing2.svg'
 
 function Landing() {
     const navigate = useNavigate();
@@ -14,23 +15,19 @@ function Landing() {
                     <p className="py-6">Improving Sustainability, Animal Welfare, and Local Economy One Farmer at a Time.</p>
                     <button className="btn text-white" onClick={() => { navigate('/home') }}>Get Started</button>
                 </div>
-                <section className='flex flex-col gap-20'>
-                    <div className="flex flex-col gap-6">
-                        <p className='text-3xl font-bold'>Why Local Farmers? 💭</p>
+                <section className='flex flex-col gap-10 mb-20'>
+                    <div className="flex flex-row items-center justify-around gap-4 w-full">
                         <div>
-                            <p>By building FarmFellow, our goal is to increase awareness whilst providing an intuitive solution in an industry that is oftentimes underappreciated and carries a large potential in sustainability. Currently, there are no solutions to support our local farmers directly except for weekly farmer's markets. However, many of these events are not known to the public and difficult for consumers to partake.</p>
-                            <br />
-                            <p>Supporting local farmers is a largely impactful act that has a tremendous effect on not only the environment, but with welfare practices and the local economy. Firstly, it reduces the environmental impact of food transportation, which is a major source of greenhouse gas emissions and climate change. Furthermore, it helps preserve the biodiversity and soil health of local ecosystems, which are often threatened by industrial agriculture and monoculture. Finally, it strengthens the social and economic fabric of local communities, which rely on farming as a source of income and food security. By supporting local farmers, we can enjoy fresh, nutritious, and diverse food while also protecting the planet and the people who grow it! 🧑‍🌾
-                            </p>
+                            <img src={Landing2} alt="Care" className="items-end" />
                         </div>
-                    </div>
-                    <div className="flex flex-col gap-6">
-                        <p className='text-3xl font-bold'>Revolutionizing Sustainability 🌳</p>
-                        <p>By supporting your local farms, you remove the middle-man and eliminate the need for large motor vehicles such as 18-wheelers and cargo planes. In chain grocery stores, we see lots of the same fruits and vegetables year-round which is an indicator of large chains mass producing and shipping these products globally. By supporting local, seasonal produce, we can slowly reduce carbon emission, one step at a time!</p>
-                    </div>
-                    <div className="flex flex-col gap-6">
-                        <p className='text-3xl font-bold'>Impacting Sustainable Practices and Animal Welfare 🌱</p>
-                        <p>Large corporations must prioritize maximizing output and profitability, which leads to inhumane processes such as steroid use and high-stress environments for cattle. Additionally, consumers will unknowingly ingest herbicides and pesticides that these large farms use to protect their crops, leading to poor soil quality and therefore, increased emissions.</p>
+                        <div className="flex flex-col gap-6 text-left">
+                            <p className='text-left text-3xl font-bold'>Why Local Farmers? 💭</p>
+                            <p className="text-left">FarmFellow aims to raise awareness and provide a solution for supporting underappreciated local farmers, enhancing sustainability.  Currently, there are no solutions to support our local farmers directly except for weekly farmer's markets. However, many of these events are not known to the public and difficult for consumers to partake.</p>
+                            <p className='text-3xl font-bold'>Revolutionizing Sustainability 🌳</p>
+                            <p>Supporting local farms eliminates middlemen and large transport vehicles, reducing carbon emissions and promoting seasonal produce.</p>
+                            <p className='text-3xl font-bold'>Impacting Sustainable Practices and Animal Welfare 🌱</p>
+                            <p>Unlike large corporations, local farms avoid inhumane practices and harmful chemicals, promoting healthier soil and reduced emissions.</p>
+                        </div>
                     </div>
                 </section>
             </div>
